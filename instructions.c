8702b5332085b723e7008c946010ad5817df5ba7
Lea Ben Zvi <leagirsh@gmail.com>
2021-07-04 21:40:10 +0300

    Update instructions.c

--- instructions.c
+++ instructions.c
@@ -1,7 +1,7 @@
 /*      Assembler Project
  *
- *      Created By:     Daniel de-Mayo  2004545346
- *                      Lea Ben Zvi     313222333
+ *      Created By:     Daniel de-Mayo
+ *                      Lea Ben Zvi
  *
  *      File Name:      instructions.c
  *      Description:    Includes all functions relevant for instruction processing.
