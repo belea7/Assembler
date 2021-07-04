d312947b752ab5cda4eee6e7fe024dd6396b69ce
Lea Ben Zvi <leagirsh@gmail.com>
2021-07-04 21:44:12 +0300

    Update create_ext_ent_files.c

--- create_ext_ent_files.c
+++ create_ext_ent_files.c
@@ -52,15 +52,16 @@ extern unsigned int digit_arr[];
 /********************************************************************************************/
 
 void create_entries_file (char * file_name) {
-    FILE * fp;
-    char *entries_file_name = (char *)malloc(sizeof(file_name)+5);      /* Stores the name of the .ent file */
-    char * address_32base = (char *) malloc (sizeof(3));                /* Stores the address in 32 base */
-    int i, count = 0 /* Counter to indicate if no .entry declarations in file */;
+    FILE* fp;
+    char* entries_file_name = (char *)malloc(sizeof(file_name)+5);      /* Stores the name of the .ent file */
+    char* address_32base = (char *) malloc (sizeof(3));                 /* Stores the address in 32 base */
+    int i, count = 0                                                    /* Counter to indicate if no .entry declarations in file */;
     union_literal address;                                              /* Stores the address to print */
 
     /* Open the .ent file */
     sprintf(entries_file_name,"%s.ent",file_name);
     fp = fopen(entries_file_name,"w");
+    
     /* If could not open entries file */
     if (!fp) {
         printf("\nERROR - Cannot create entries file\n");
@@ -79,6 +80,7 @@ void create_entries_file (char * file_name) {
     }
     free(address_32base);
     fclose(fp);
+    
     /* If not entries found - remove empty entries file*/
     if (!count)
         remove(entries_file_name);
@@ -90,12 +92,13 @@ void create_externals_file (char * file_name) {
     char extern_name [MAX_LABEL_NAME];                                  /* Stores the name of the extern label */
     char * address_32base = (char *) malloc (sizeof(3));                /* Stores the address in 32 base */
     char * externals_file_name = (char *)malloc(sizeof(file_name)+5);   /* Stores the name of the .ext file */
-    int i, j, count = 0 /* Counter to indicate if no .extern declarations in file */;
+    int i, j, count = 0                                                 /* Counter to indicate if no .extern declarations in file */;
     union_literal address;                                              /* Stores the address to print */
 
     /* Open the .ext file */
     sprintf(externals_file_name,"%s.ext",file_name);
     fp = fopen(externals_file_name,"w");
+    
     /* If could not open externals file */
     if (!fp) {
         printf("\nERROR - Cannot create externals file\n");
@@ -107,6 +110,7 @@ void create_externals_file (char * file_name) {
         /* If found .extern declaration */
         if (!strcmp(data_lines[i].type, ".extern")) {
             strcpy(extern_name, data_lines[i].name);
+            
             /* Go through all instructions declarations */
             for (j=0; j < instructions_index; j++) {
                 /* If source operand name is equal to external label name */
@@ -116,18 +120,20 @@ void create_externals_file (char * file_name) {
                     sprintf(address_32base, "%c%c", digit_arr[address.lit32.first_digit], digit_arr[address.lit32.second_digit]);
                     fprintf(fp, "%s\t%s\n", extern_name, address_32base);
                 }
+                
                 /* If destination operand name is equal to external label name */
                 if (!strcmp(instructions[j].dest_operand_name, extern_name)) {
                     count++;
                     if ((int)strlen(instructions[j].src_operand_name) == NO) {
                         address.n32.num = (unsigned) (instructions[j].cur_ic + 1);
                     }
-                        /* If first addressing type is not empty and is struct*/
+                    /* If first addressing type is not empty and is struct*/
                     else if (is_struct(instructions[j].src_operand_name)) {
                         /* Source operand code is two lines after instruction line */
                         address.n32.num = (unsigned) (instructions[j].cur_ic + 3);
                     }
-                        /* If addressing type is not struct  - 1 line after instruction line */
+                    
+                    /* If addressing type is not struct  - 1 line after instruction line */
                     else  {
                         address.n32.num = (unsigned)(instructions[j].cur_ic + 2);
                     }
@@ -139,6 +145,7 @@ void create_externals_file (char * file_name) {
     }
     free(address_32base);
     fclose(fp);
+    
     /* If not entries found - remove empty entries file*/
     if (!count)
         remove(externals_file_name);
